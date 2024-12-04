dfdb0b9a54ee7bc3c2e4e7263640fa3ce9930915
Josh Goldberg <git@joshuakgoldberg.com>
2024-12-04 15:45:17 -0500

    feat: more informative page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,13 @@ function App() {
           <img src={reactLogo} className="logo react" />
         </a>
       </div>
-      <h1>Accessibility Mob Programming</h1>
+      <h1>Accessibility (&ldquo;a11y&rdquo;) Mob Programming</h1>
+      <p className="description">
+        A quick app showing a bunch of accessibility issues. 🦾
+        <br />
+        We&lsquo;re going to &ldquo;mob program&rdquo; (you all tell me what to
+        type) to fix them!
+      </p>
       <div className="card">
         <h3>Clickers</h3>
         <div className="clickers">
@@ -30,6 +36,13 @@ function App() {
       <p className="instructions">
         Edit <code>src/App.jsx</code> and save to update the page
       </p>
+      <a
+        className="source"
+        href="https://github.com/JoshuaKGoldberg/a11y-mob-programming"
+        target="_blank"
+      >
+        view source on GitHub
+      </a>
     </>
   );
 }
